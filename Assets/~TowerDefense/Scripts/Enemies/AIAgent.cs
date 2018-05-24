9a49befaf38956266ad577b50f0ffff42250dbed
daniel.sy2 <daniel.sy2@600900B211DE402>
2018-05-24 15:40:20 +1000

    added scripts

---
+++ Assets/~TowerDefense/Scripts/Enemies/AIAgent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+namespace TowerDefence
+{
+    public class AIAgent : MonoBehaviour
+    {
+        public Transform target;
+        private NavMeshAgent nav;
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+        void Update()
+        {
+            if(target)
+            {
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
---
+++ Assets/~TowerDefense/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TowerDefence
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float health = 100f;
+
+        public void DealDamage(float damage)
+        {
+            // 
+            health -= damage;
+            //
+            if (health <= 0)
+            {
+                //
+                Destroy(gameObject);
+            }
+        }
+    }
+}
